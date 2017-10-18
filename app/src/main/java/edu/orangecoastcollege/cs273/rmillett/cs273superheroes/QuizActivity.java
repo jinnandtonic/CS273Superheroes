1abf7c2e8bfd12c3b8bc611ed5e848c27f96fe51
RyanMillett <rmillett@student.cccd.edu>
2017-10-18 13:32:21 -0700

    Built Superhero.java model class and started QuizActivity.java

--- app/src/main/java/edu/orangecoastcollege/cs273/rmillett/cs273superheroes/QuizActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/rmillett/cs273superheroes/QuizActivity.java
@@ -1,13 +1,74 @@
 package edu.orangecoastcollege.cs273.rmillett.cs273superheroes;
 
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <code>QuizActivity</code> is an interactive quiz that displays random images of CS273 students
+ * drawn from a database and prompts the user to match the correct name, super power, or piece of
+ * trivia depending on which mode of play has been selected
+ */
 public class QuizActivity extends AppCompatActivity {
 
+    public static final String TAG = "Superhero Quiz";
+    public static final int QUESTIONS_IN_QUIZ = 10;
+
+    private Button[] mButtons = new Button[4];
+    private LinearLayout[] mLinearLayout = new LinearLayout[2];
+
+    // TODO: create Superhero model class
+    private List<Superhero> mAllSuperheroesList;
+    private List<Superhero> mQuizSuperheroesList;
+    private List<Superhero> mFilteredSuperheroesList;
+
+    private Superhero mCorrectSuperhero;
+
+    private int mTotalGuesses;
+    private int mCorrectGuesses;
+
+    private SecureRandom rng;
+
+    private Handler handler;
+
+    private TextView mQuestionNumberTextView;
+    private ImageView mSuperheroImageView;
+    private TextView mAnswerTextView;
+
+    private String mMode;
+
+    public static final String MODES = "modes";
+
+    /**
+     * Creates an instance of <code>QuizActivity</code> in the view
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+        // TODO: build mPreferenceChangeListener class
+        // Register the OnSharedPreferencesChangeListener
+//        SharedPreferences mPreferences = android.preference.PreferenceManager.getDefaultSharedPreferences(this);
+//        mPreferences.registerOnSharedPreferenceChangeListener(mPreferenceChangeListener);
+
+        mQuizSuperheroesList = new ArrayList<>(QUESTIONS_IN_QUIZ);
+        rng = new SecureRandom();
+        handler = new Handler();
+
+
     }
 }
---
+++ app/src/main/java/edu/orangecoastcollege/cs273/rmillett/cs273superheroes/Superhero.java
@@ -0,0 +1,128 @@
+package edu.orangecoastcollege.cs273.rmillett.cs273superheroes;
+
+/**
+ * Represents a Superhero for the purposes of <code>QuizActivity</code>, including the superhero's
+ * name, superpower, piece of trivia, and file name
+ *
+ * @author Ryan Millett.
+ * @version 1.0
+ */
+public class Superhero {
+
+    private String mUsername;
+    private String mName;
+    private String mSuperpower;
+    private String mOneThing;
+    private String mFileName;
+
+    /**
+     * Instantiates a new <code>Superhero</code> given its username, name, superpower, and one thing.
+     * @param username The username of the <code>Superhero</code>
+     * @param name The name of the <code>Superhero</code>
+     * @param superpower The superpower of the <code>Superhero</code>
+     * @param oneThing One piece of trivia about the <code>Superhero</code>
+     */
+    public Superhero(String username, String name, String superpower, String oneThing) {
+        mUsername = username;
+        mName = name;
+        mSuperpower = superpower;
+        mOneThing = oneThing;
+        name = name.replaceAll(" ", "_");
+        superpower = superpower.replaceAll(" ", "_");
+        oneThing = oneThing.replaceAll(" ", "_");
+        mFileName = username + ".png";
+    }
+
+    /**
+     * Gets the username of the <code>Superhero</code>
+     * @return The username of the <code>Superhero</code>
+     */
+    public String getUsername() {
+        return mUsername;
+    }
+
+    /**
+     * Gets the name of the <code>Superhero</code>
+     * @return The name of the <code>Superhero</code>
+     */
+    public String getName() {
+        return mName;
+    }
+
+    /**
+     * Gets the superpower of the <code>Superhero</code>
+     * @return The superpower of the <code>Superhero</code>
+     */
+    public String getSuperpower() {
+        return mSuperpower;
+    }
+
+    /**
+     * Gets the one thing of the <code>Superhero</code>
+     * @return The one thing of the <code>Superhero</code>
+     */
+    public String getOneThing() {
+        return mOneThing;
+    }
+
+    /**
+     * Gets the file name of the <code>Superhero</code>
+     * @return The file name of the <code>Superhero</code>
+     */
+    public String getFileName() {
+        return mFileName;
+    }
+
+    /**
+     * Compares two <code>Superhero</code> objects for equality based on username, name, superpower,
+     * one thing, and file name.
+     * @param o The other <code>Superhero</code>
+     * @return True if the <code>Superhero</code> objects are the same, false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Superhero superhero = (Superhero) o;
+
+        if (mUsername != null ? !mUsername.equals(superhero.mUsername) : superhero.mUsername != null)
+            return false;
+        if (mName != null ? !mName.equals(superhero.mName) : superhero.mName != null) return false;
+        if (mSuperpower != null ? !mSuperpower.equals(superhero.mSuperpower) : superhero.mSuperpower != null)
+            return false;
+        if (mOneThing != null ? !mOneThing.equals(superhero.mOneThing) : superhero.mOneThing != null)
+            return false;
+        return mFileName != null ? mFileName.equals(superhero.mFileName) : superhero.mFileName == null;
+
+    }
+
+    /**
+     * Generates an integer based hash code to uniquely represent this <code>Superhero</code>.
+     * @return An integer based hash code to represent this <code>Superhero</code>.
+     */
+    @Override
+    public int hashCode() {
+        int result = mUsername != null ? mUsername.hashCode() : 0;
+        result = 31 * result + (mName != null ? mName.hashCode() : 0);
+        result = 31 * result + (mSuperpower != null ? mSuperpower.hashCode() : 0);
+        result = 31 * result + (mOneThing != null ? mOneThing.hashCode() : 0);
+        result = 31 * result + (mFileName != null ? mFileName.hashCode() : 0);
+        return result;
+    }
+
+    /**
+     * Generates a text based representation of this <code>Superhero</code>.
+     * @return A text based representation of this <code>Superhero</code>.
+     */
+    @Override
+    public String toString() {
+        return "Superhero{" +
+                "mUsername='" + mUsername + '\'' +
+                ", mName='" + mName + '\'' +
+                ", mSuperpower='" + mSuperpower + '\'' +
+                ", mOneThing='" + mOneThing + '\'' +
+                ", mFileName='" + mFileName + '\'' +
+                '}';
+    }
+}
